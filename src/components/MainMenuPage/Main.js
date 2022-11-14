d7305b0e87a8a75d2c429d9aadf2c50a22617cc9
BrightWisdom0502 <brightwisdom0502@gmail.com>
2022-11-14 20:53:14 +0900

    react-bootstrap 적용

--- src/components/MainMenuPage/Main.js
+++ src/components/MainMenuPage/Main.js
@@ -1,10 +1,13 @@
 import ForCustomer from "./ForCustomer";
 import ForAdmin from "./ForAdmin";
 
+import Button from "react-bootstrap/Button";
+
 const Main = () => {
   return (
     <>
       <h1>상상 카페</h1>
+      <Button variant="primary">react-bootstrap 테스트</Button>
       <ForCustomer />
       <ForAdmin />
     </>
