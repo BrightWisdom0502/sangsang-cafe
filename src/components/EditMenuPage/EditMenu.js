7540d19e14a6f2467b5054c775374c7cb4c6b055
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-25 15:39:16 +0900

    Update EditMenu.js

--- src/components/EditMenuPage/EditMenu.js
+++ src/components/EditMenuPage/EditMenu.js
@@ -1,10 +1,21 @@
+import { Route, Link } from "react-router-dom";
+import { React, useState, useSelector } from "react";
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 import MenuCategory from "../MenuPage/MenuCategory";
 import SearchBar from "../MenuPage/SearchBar";
 import Menus from "../MenuPage/Menus";
 import MenuControl from "./MenuControl.js";
 
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import beverage from "../../data/beverage.json";
+import coffee from "../../data/coffee.json";
+import desserts from "../../data/desserts.json";
+import tea from "../../data/tea.json";
+
+import MenuChange from "./MenuChange";
+import CategoryButton from "./CategoryButton";
 
 const ComponentStyle = {
   display: "flex",
@@ -14,9 +25,12 @@ const ComponentStyle = {
   backgroundColor: "#A4D3A0"
 };
 
-const ListStyle = {
-  height: "100px",
-  textAlign: "left"
+const ButtonStyle = {
+  textAlign: "center",
+  height: "35px",
+  borderColor: "#587558" , 
+  marginBottom: "20px"
+
 }
 
 const CardStyle = {
@@ -25,7 +39,8 @@ const CardStyle = {
   overflow: "hidden",
 }
 
-const ButtonStyle = {
+
+const CategoryStyle = {
   width: "170px",
   height: "40px",
   marginLeft: "15px",
@@ -34,105 +49,63 @@ const ButtonStyle = {
 }
 
 
-const EditMenu = () => {
+const LinkStyle = {
+  color: "white",
+}
+
+
+
+
+
+
+
+const EditMenu = (props) => {
+
+  var [menuName, setMenuName] = React.useState(useSelector((state) => state.menuName));
+  var [menuPrice, setMenuPrice] = React.useState(useSelector((state) => state.menuPrice));
+  var [menuText, setMenuText] = React.useState(useSelector((state) => state.menuText));
+
+  const updateData = async (e) => {
+    e.preventDefault();
+    var data = {menuName, menuPrice, menuText};
+    props.props();
+  }
+
+
+
+
   return (
+
+
     <div id="EditMenu" style={ComponentStyle}>
 
     
 
-      <div class="d-grid gap-2 d-md-block position-absolute">
-        <button class="btn btn-primary" type="button" style={ButtonStyle}>카테고리</button>
-        <button class="btn btn-primary" type="button" style={ButtonStyle}>카테고리</button>
-        <button class="btn btn-primary" type="button" style={ButtonStyle}>카테고리</button>
-        <button class="btn btn-primary" type="button" style={ButtonStyle}>카테고리</button>
-        <button class="btn btn-primary" type="button" style={ButtonStyle}>카테고리</button>
+      <div className="d-grid gap-2 d-md-block position-absolute">
+        <nav>
+        <button className="btn btn-primary" type="button" style={CategoryStyle}  >beverage</button>
+        <button className="btn btn-primary" type="button" style={CategoryStyle}  >coffee</button>
+        <button className="btn btn-primary" type="button" style={CategoryStyle}  >desserts</button>
+        <button className="btn btn-primary" type="button" style={CategoryStyle}  >tea</button>
+        </nav>
       </div>
 
 
+      <Card className="row row-cols-1 row-cols-md-3 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
+                <div className="col" >
+                <div className="card h-100" style={CardStyle}>
+                    <img src={ "그린 티.jpg" } className="card-img-top" alt="..." /> 
+                    <div className="card-body">
+                        <h5 className="card-title" type="text"  id="menuName" value={menuName} onChange={(e) => setMenuName(e.target.value)} />  
+                        <h4 type="number" id="menuPrice" value={menuPrice} onChange={(e) => setMenuPrice(e.target.value)} 원 />
+                        <p className="card-text" type="text" id="FormText" value={menuText} onChange={(e) => setMenuText(e.target.value)} />
+                    <a href="#" className="btn btn-outline stretched-link" style={ButtonStyle}>편집</a>
+                    </div>
+                </div>
+                </div>
+      </Card>
 
-
-      <div class="row row-cols-1 row-cols-md-3 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." /> 
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      
 
 
       <div style={{ width: '30vh' , height: '80vh' , marginTop: '70px' ,marginLeft: '55vw', position: 'absolute'}}>
@@ -140,7 +113,7 @@ const EditMenu = () => {
       </div>
         
         <>
-    
+      
       <MenuCategory />
       <SearchBar />
       <Menus />
