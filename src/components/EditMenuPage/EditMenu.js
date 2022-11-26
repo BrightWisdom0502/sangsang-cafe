7e40de8185b9eb78761a1509b769d20bda049e00
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-26 21:06:31 +0900

    Update EditMenu.js

--- src/components/EditMenuPage/EditMenu.js
+++ src/components/EditMenuPage/EditMenu.js
@@ -1,22 +1,23 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link, Route, Switch } from "react-router-dom";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 
 import MenuCategory from "../MenuPage/MenuCategory";
-import SearchBar from "../MenuPage/SearchBar";
 import Menus from "../MenuPage/Menus";
 import MenuControl from "./MenuControl.js";
 
-import beverage from "../../data/beverage.json";
-import coffee from "../../data/coffee.json";
-import desserts from "../../data/desserts.json";
-import tea from "../../data/tea.json";
+import beverage from "./beverage";
+import coffee from './coffee';
+import desserts from './desserts';
+import tea from './tea';
 
-// import MenuChange from "./MenuChange";
-// import MenuCard from "./MenuCard";
+import Edit001 from "./Edit001";
+import Edit002 from "./Edit002";
+import Edit003 from "./Edit003";
+import Edit004 from "./Edit004";
 
 
 const ComponentStyle = {
@@ -56,17 +57,6 @@ const CategoryStyle = {
 
 const EditMenu = (props) => {
   
-  const [menuName, setMenuName] = React.useState(useSelector((state) => state.menuName));
-  const [menuPrice, setMenuPrice] = React.useState(useSelector((state) => state.menuPrice));
-  const [menuText, setMenuText] = React.useState(useSelector((state) => state.menuText));
-
-  const updateData = async (e) => {
-    e.preventDefault();
-    var data = {menuName, menuPrice, menuText};
-    props.props();
-  }
-
-
 
 
   return (
@@ -75,40 +65,48 @@ const EditMenu = (props) => {
     <div id="EditMenu" style={ComponentStyle}>
 
     
-
+      
       <div className="d-grid gap-2 d-md-block position-absolute">
         <nav>
         <button className="btn btn-primary" type="button" style={CategoryStyle}>
-          <Link to = "Edit001" style={{color: "white"}}>beverage</Link>
+          <Nav.Link as={Link} to = "/Edit001" style={{color: "white"}}>beverage</Nav.Link>
         </button>
         <button className="btn btn-primary" type="button" style={CategoryStyle}>
-          <Link to = "Edit001" style={{color: "white"}}>coffee</Link>
+          <Nav.Link as={Link} to = "/Edit002" style={{color: "white"}}>coffee</Nav.Link>
         </button>
         <button className="btn btn-primary" type="button" style={CategoryStyle}>
-          <Link to = "Edit001" style={{color: "white"}}>desserts</Link>
+          <Nav.Link as={Link} to = "/Edit003" style={{color: "white"}}>desserts</Nav.Link>
         </button>
         <button className="btn btn-primary" type="button" style={CategoryStyle}>
-          <Link to = "Edit001" style={{color: "white"}}>tea</Link>
+          <Nav.Link as={Link} to = "/Edit004" style={{color: "white"}}>tea</Nav.Link>
         </button>
         </nav>
       </div>
 
 
-      <>
-      <div className="row row-cols-1 row-cols-md-3 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
-                <div className="col" >
-                <Card className="card h-100" style={CardStyle}>
-                    <img src={ "그린 티.jpg" } className="card-img-top" alt="..." /> 
-                    <div className="card-body">
-                        <h5 className="card-title" type="text"  id="menuName" value={menuName} onChange={(e) => setMenuName(e.target.value)} />  
-                        <h4 type="number" id="menuPrice" value={menuPrice} onChange={(e) => setMenuPrice(e.target.value)} 원 />
-                        <p className="card-text" type="text" id="FormText" value={menuText} onChange={(e) => setMenuText(e.target.value)} />
-                    <a href="#" className="btn btn-outline stretched-link" style={ButtonStyle}>편집</a>
-                    </div>
-                </Card>
-                </div>
-      </div>
-      </>
+
+      <div className="row row-cols-1 row-cols-md-4 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
+        
+        
+        <div className="col">
+          {
+              beverage.map(function(n, i) {
+                  return (
+                      <Card beverage={beverage[i]} i={i} key={i} />
+                  )
+              })
+          }
+        </div>
+
+        <Card className="card h-100" style={CardStyle}>
+          <div className="col-md-4">
+            <img src={'images/그린 티.jpg'} width="100%" />
+            <h3> 제목 </h3>
+            <p> 가격</p>
+            <a href="#" className="btn btn-outline stretched-link" style={ButtonStyle}>편집</a>
+          </div>
+        </Card>
+
 
       
 
@@ -117,12 +115,13 @@ const EditMenu = (props) => {
         <MenuControl />
       </div>
         
-        <>
-      
+
+    
+        <> 
       <MenuCategory />
-      <SearchBar />
       <Menus />
         </>
+    </div>
     </div>
 
   );
