dbe5d38840db8a8fa94219955a2230c7f2d4417a
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-26 12:47:41 +0900

    Update Edit004.js

--- src/components/EditMenuPage/Edit004.js
+++ src/components/EditMenuPage/Edit004.js
@@ -1,11 +1,85 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, NavLink } from "react-router-dom";
 
-import React from 'react'
+
+const CardStyle = {
+  width: "230px",
+  height: "350px",
+  overflow: "hidden",
+}
+
+const ButtonStyle = {
+  textAlign: "center",
+  height: "35px",
+  borderColor: "#587558" , 
+  marginBottom: "20px"
+
+}
 
 const Edit004 = () => {
   return (
-    <div>Edit004</div>
+      <div class="row row-cols-1 row-cols-md-3 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
+        <div class="col" style={CardStyle}>
+          <div class="card h-100">
+            <img src="images/그린 티.jpg" class="card-img-top" alt="..." /> 
+            <div class="card-body">
+              <h5 class="card-title">그린 티 5000\</h5>
+              <p class="card-text">메뉴 설명</p>
+              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col" style={CardStyle}>
+          <div class="card h-100">
+            <img src="images/민트 티.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">민트 티 4500</h5>
+              <p class="card-text">메뉴 설명</p>
+              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col" style={CardStyle}>
+          <div class="card h-100">
+            <img src="images/밀크 티.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">밀크 티 5500</h5>
+              <p class="card-text">메뉴 설명</p>
+              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col" style={CardStyle}>
+          <div class="card h-100">
+            <img src="images/얼그레이 티.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">얼그레이 티 4500</h5>
+              <p class="card-text">메뉴 설명</p>
+              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col" style={CardStyle}>
+          <div class="card h-100">
+            <img src="images/캐모마일 티.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">캐모마일 티 4500</h5>
+              <p class="card-text">메뉴 설명</p>
+              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+      
+    
   )
 }
 
-export default Edit004
+export default Edit004;
