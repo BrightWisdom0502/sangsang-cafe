1e1b3a870692fbd5ba67316718ba3c3af1bd870d
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-26 12:49:08 +0900

    Update MenuControl.js

--- src/components/EditMenuPage/MenuControl.js
+++ src/components/EditMenuPage/MenuControl.js
@@ -2,7 +2,7 @@ import { React, useState, useSelector } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-import  MenuForm, { values, handleChange, handleSubmit } from './MenuForm';
+import  MenuForm from './MenuForm';
 
 
 const AdminMenuStyle = {
@@ -35,34 +35,9 @@ const MenuControl = ({init}) => {
             <p className="card-text">상품 편집 페이지입니다.</p>
 
             <form action="" className="EditCard">
-              <div className="md-input-group">
-                  <label for="FormName" className="form-label">상품명</label>
-                  <input type="text"  id="menuName" />
-              </div>
-        
-              <div className="input-group">
-                <label for="FormPrice" className="form-label">가격
-                <input type="number" id="menuPrice" />
-                </label>
-              </div>
-        
-              <div className="col-input-text-group">
-                <label for="FormText" className="form-label">상품설명</label>
-                <input type="text" id="FormText" />
-              </div>
-
-              <div className="col-md-4">
-                <label for="inputState" className="form-label">상품 공개 여부</label>
-                <select id="inputState" className="form-select">
-                  <option value="공개" selected>공개</option>
-                  <option value="비공개">비공개</option>
-                </select>
-              </div>
-              
-              
-              <div className="col-12">
-                <button type="submit" className="btn btn-primary" style={ButtonStyle}>수정</button>
-              </div>
+              <>
+              { MenuForm }
+              </>
             </form>
           </div>
         </div>
