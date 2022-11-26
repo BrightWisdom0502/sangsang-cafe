a24c9b1aa00406a2b8183331bae20f282181f58c
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-26 12:46:22 +0900

    Update Edit001.js

--- src/components/EditMenuPage/Edit001.js
+++ src/components/EditMenuPage/Edit001.js
@@ -20,9 +20,9 @@ const Edit001 = () => {
       <div class="row row-cols-1 row-cols-md-3 g-3" style={{ marginTop: '40px', marginLeft: '10px' , maxWidth: '60vw'}}>
         <div class="col" style={CardStyle}>
           <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." /> 
+            <img src="images/녹차 라떼.jpg" class="card-img-top" alt="..." /> 
             <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
+              <h5 class="card-title">녹차 라떼 5500\</h5>
               <p class="card-text">메뉴 설명</p>
               <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
             </div>
@@ -31,9 +31,9 @@ const Edit001 = () => {
 
         <div class="col" style={CardStyle}>
           <div class="card h-100">
-            <img src="images/그린 티.jpg" class="card-img-top" alt="..." />
+            <img src="images/핫 초콜릿.jpg" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
+              <h5 class="card-title">핫 초콜릿 5000</h5>
               <p class="card-text">메뉴 설명</p>
               <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
             </div>
@@ -42,71 +42,17 @@ const Edit001 = () => {
 
         <div class="col" style={CardStyle}>
           <div class="card h-100">
-            <img src="images/민트 티.jpg" class="card-img-top" alt="..." />
+            <img src="images/아이스 초콜릿.jpg" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
+              <h5 class="card-title">아이스 초콜릿 5300</h5>
               <p class="card-text">메뉴 설명</p>
               <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
             </div>
           </div>
         </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/밀크 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/바닐라 라떼.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/베이글.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/스콘.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col" style={CardStyle}>
-          <div class="card h-100">
-            <img src="images/아이스 그린 티.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">메뉴 이름</h5>
-              <p class="card-text">메뉴 설명</p>
-              <a href="#" class="btn btn-outline" style={ButtonStyle}>편집</a>
-            </div>
-          </div>
-        </div>
-
       </div>
+
+
 
       
     
