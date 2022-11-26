e63160ed68e7ad8d0081b7741e7d36b7d45d0b0b
syyyl <56541363+syyyl@users.noreply.github.com>
2022-11-26 12:49:29 +0900

    Update MenuForm.js

--- src/components/EditMenuPage/MenuForm.js
+++ src/components/EditMenuPage/MenuForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 
-
-
-function MenuForm ({ initalValues, onSubmit }) {
+const MenuForm = ({init}) => {
 
     
-        const [values, setValues] = useState(initalValues)
+        const [values, setValues] = useState({
+            menuName: "",
+            menuPrice: "",
+            menuText: "",
+        })
 
         const handleChange = e => {
             setValues({
@@ -17,16 +19,59 @@ function MenuForm ({ initalValues, onSubmit }) {
         const handleSubmit = e => {
             e.preventDefault()
             
-            onSubmit(values)
         }
 
+        const ButtonStyle = {
+            textAlign: "center",
+            height: "35px",
+            borderColor: "#587558" , 
+            marginBottom: "20px"
+          
+          }
 
 
-    return {
-        values,
-        handleChange,
-        handleSubmit,
-    }
+
+        return (
+            <>
+            <div className="md-input-group">
+                <input 
+                    type="text"  
+                    id="menuName" 
+                    value={values.menuName}
+                    onChange={ handleChange }
+                    />
+            </div>
+
+            <div className="input-group">
+                <label for="FormPrice" className="form-label">가격
+                <input 
+                  type="number" 
+                  id="menuPrice" 
+                  value={values.menuPrice}
+                  onChange={ handleChange }
+                  />
+                </label>
+              </div>
+        
+              <div className="col-input-text-group">
+                <label for="FormText" className="form-label">상품설명</label>
+                <input 
+                  type="text" 
+                  id="FormText" 
+                  value={values.menuText}
+                  onChange={ handleChange }
+                  />
+              </div>
+
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary" style={ButtonStyle}>수정</button>
+              </div>
+            
+
+            </>
+
+        );
+
 }
 
 export default MenuForm;
